1f26df953b0932dce8051d6204e45b75032b90b1
elizmey <siomeyromero@gmail.com>
2025-09-02 09:44:20 -0500

    cambios
    errores al ingresar usuarios repetidos y agregar menu

--- tasks/views.py
+++ tasks/views.py
@@ -1,29 +1,46 @@
-# filepath: c:\Users\Admin\Documents\ProyectosPythonS\djangoCrudS\tasks\views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 
+# Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
+def tasks(request):
+    return render(request, "tasks.html")
 
 def signup(request):
-    return render(request, 'signup.html')
-
-def signup(request):
-    if request.method =="GET":
-        return render(request,
-                      "signup.html",
-                      {"form":UserCreationForm})
+    if request.method == "GET":
+        return render(request, 
+                  "signup.html",
+                  {"form":UserCreationForm})
     else:
-        if request.POST["password1"]==request.POST["password2"]:
+        if request.POST["password1"] == request.POST["password2"]:
             try:
-               user = User.objects.create_user(username=request.POST["username"],
-                                         password=request.POST["password1"])
-               user.save()
-               return HttpResponse("el usuario creado correctamente")
-            except Exception as e:
-                return HttpResponse(f"Error al crear el usuario: {e}")
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect("tasks")
+
+            except IntegrityError:
+                return render(request,
+                              'signup.html',
+                              {"form": UserCreationForm(),
+                               "error":"Error al crear el usuario"})
+                
         else:
-            return HttpResponse("las contraseñas no coinciden")
-        
+                 return render(request,
+                              'signup.html',
+                              {"form": UserCreationForm(),
+                               "error":"Error, Las contraseñas no coinciden"})
+
+
+
+
+
+
+
